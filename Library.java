68cf0e164aa0a080a37cd388cc70424b6565fc8d
Connor Downey <connordowney6@gmail.com>
2021-02-18 12:16:17 -0500

    adding frameworks

---
+++ Library.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+public class Library {
+    private HashMap<String, Integer> books;
+    private static Library library;
+
+    private Library()
+    {
+
+    }
+
+    public static Library getInstance()
+    {
+
+    }
+
+    public boolean checkoutBook(String bookName)
+    {
+
+    }
+
+    public void checkInBook(String bookName, int numToAdd)
+    {
+
+    }
+
+    public void displayBooks()
+    {
+
+    }
+}
