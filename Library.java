561ced003a8b75c53369f20742d1003155bf1d98
Connor Downey <connordowney6@gmail.com>
2021-02-20 15:30:06 -0500

    adding completed code with output and comments

--- Library.java
+++ Library.java
@@ -1,27 +1,40 @@
 import java.util.HashMap;
 
+/**
+ * A Library
+ * @author Connor Downey
+ */
 public class Library {
-    private HashMap<String, Integer> books;
+    private HashMap<String, Integer> books = new HashMap<String,  Integer>();
     private static Library library;
 
     private Library() {}
 
+    /**
+     * Creates an instance of a library if there isn't one
+     * @return The instance of the library
+     */
     public static Library getInstance()
     {
         if(library==null)
         {
-            System.out.println("Creating our Library. Time to begin reading");
+            System.out.println("Creating our Library. Time to begin reading.");
             library = new Library();
         }
         return library;
     }
 
+    /**
+     * Checks out the book
+     * @param bookName Name of the book
+     * @return True if the book is checked out, False if there were no copies of the book
+     */
     public boolean checkoutBook(String bookName)
     {
         if(books.containsKey(bookName) && books.get(bookName)>0)
         {
-            books.remove(bookName);
-            System.out.println("...was successfully checkout out");
+            books.put(bookName, 0);
+            System.out.println(bookName+" was successfully checked out");
             return true;
         }
         else
@@ -31,11 +44,17 @@ public class Library {
         }
     }
 
+    /**
+     * Checks in a book
+     * @param bookName Name of the book
+     * @param numToAdd Number of books being checked in
+     */
     public void checkInBook(String bookName, int numToAdd)
     {
         if(books.containsKey(bookName))
         {
             books.put(bookName, books.get(bookName)+numToAdd);
+            System.out.print("A new copy of ");
         }
         else
         {
@@ -44,6 +63,9 @@ public class Library {
         System.out.println(bookName+" was added to the library");
     }
 
+    /**
+     * Displays the books in the inventory with the number of copies
+     */
     public void displayBooks()
     {
         System.out.println("Inventory:");
